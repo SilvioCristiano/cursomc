8edbbf4d9eb24a5b60a7d34e9d8c181f371453fd
Silvio Cristiano <sivuca1@gmail.com>
2018-02-03 01:12:46 -0200

    O banco de dados H2 e a criacao da base de dados

--- src/main/java/com/silvio/cursomc/domain/Categoria.java
+++ src/main/java/com/silvio/cursomc/domain/Categoria.java
@@ -1,10 +1,16 @@
 package com.silvio.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-
+@Entity
 public class Categoria implements java.io.Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	
