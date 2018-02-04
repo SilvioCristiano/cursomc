7b12e61e0da6846d23aec54d0a1ee1537a025566
Silvio Cristiano <sivuca1@gmail.com>
2018-02-04 02:35:02 -0200

    Criando repositorio e service para CATEGORIA

--- src/main/java/com/silvio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/silvio/cursomc/resources/CategoriaResource.java
@@ -1,29 +1,24 @@
 package com.silvio.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
 
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.silvio.cursomc.domain.Categoria;
+import com.silvio.cursomc.service.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> lista() {
-		
-		Categoria cat1 = new Categoria(1, "Ïnformatica");
-		Categoria cat2 = new Categoria(2, "Escritorio");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
-		
+	private CategoriaService service;
+	
+	@RequestMapping(value ="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+			
 	}
 }
---
+++ src/main/java/com/silvio/cursomc/service/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.silvio.cursomc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.silvio.cursomc.domain.Categoria;
+import com.silvio.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+
+}
