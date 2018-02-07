e9524c1221fb02a9f7df9e11402bb97f79db2907
Silvio Cristiano <sivuca1@gmail.com>
2018-02-07 00:56:44 -0200

    produtos e associacao muitos para muitos

--- src/main/java/com/silvio/cursomc/CursomcApplication.java
+++ src/main/java/com/silvio/cursomc/CursomcApplication.java
@@ -8,12 +8,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.silvio.cursomc.domain.Categoria;
+import com.silvio.cursomc.domain.Produto;
 import com.silvio.cursomc.repositories.CategoriaRepository;
+import com.silvio.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -24,6 +28,18 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Ïnformatica");
 		Categoria cat2 = new Categoria(null, "Äccount");
 		
+		Produto p1 = new Produto(null, "Computador", 2.000); 
+		Produto p2 = new Produto(null, "Impressora", 1.000); 
+		Produto p3 = new Produto(null, "Mouse", 6.0); 
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		produtoRepository.save(Arrays.asList(p1,p2,p3));
 	}
 }
--- src/main/java/com/silvio/cursomc/domain/Produto.java
+++ src/main/java/com/silvio/cursomc/domain/Produto.java
@@ -7,75 +7,84 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-//INSERT INTO CATEGORIA(NAME)VALUES('ESCRITORIO'); 
+
 @Entity
-public class Categoria implements java.io.Serializable{
-	
+public class Produto implements java.io.Serializable{
+
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	private Double preco;
+	@ManyToMany
+	@JoinTable(name="PRODUTO_CATEGORIA", 
+	joinColumns = @JoinColumn(name="produto_id"), 
+	inverseJoinColumns = @JoinColumn(name="categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	public Categoria() {
-		
-	}
-
-	public Categoria(Integer id, String name) {
+	public Produto() {}
+	public Produto(Integer id, String name, Double preco) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.preco = preco;
 	}
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-	public List<Produto> getProdutos() {
-		return produtos;
+	
+	public List<Categoria> getCategorias() {
+		return categorias;
 	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", name=" + name + ", preco=" + preco + "]";
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
-		
-		Categoria other = (Categoria) obj;
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		
 		return true;
 	}
 	
 	
-
 }
