d305ce4e559bffd57e280af3bdbfb539c4591bc3
Silvio Cristiano <sivuca1@gmail.com>
2018-02-08 01:08:59 -0200

    Inclusao de faultHandler

--- src/main/java/com/silvio/cursomc/domain/Produto.java
+++ src/main/java/com/silvio/cursomc/domain/Produto.java
@@ -11,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements java.io.Serializable{
 
@@ -20,6 +22,8 @@ public class Produto implements java.io.Serializable{
 	private Integer id;
 	private String name;
 	private Double preco;
+	
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name="PRODUTO_CATEGORIA", 
 	joinColumns = @JoinColumn(name="produto_id"), 
--- src/main/java/com/silvio/cursomc/service/CategoriaService.java
+++ src/main/java/com/silvio/cursomc/service/CategoriaService.java
@@ -1,10 +1,15 @@
 package com.silvio.cursomc.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.silvio.cursomc.domain.Categoria;
 import com.silvio.cursomc.repositories.CategoriaRepository;
+
+import com.silvio.cursomc.service.exceptions.ObjectNotFoundExcepion;
+
+
 
 @Service
 public class CategoriaService {
@@ -14,6 +19,10 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 
 		Categoria obj = repo.findOne(id);
+		
+		if(obj==null) {
+			throw new ObjectNotFoundExcepion("Objeto não encontrado! Id " + id + " Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
